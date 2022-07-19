fbb625cff060f40f8221d244f8ff1ba1e2e926a3
estherjanesamuel <esther.jane.samuel@gmail.com>
2022-07-19 17:29:10 +0700

    load dynamic image

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,14 +6,15 @@ import Rating from "@mui/material/Rating";
 import Typography from "@mui/material/Typography";
 import * as React from 'react';
 
+const BASE_IMAGE_URL = "http://image.tmdb.org/t/p/original"
 const MovieCard = ({ movie }) => {
     return (
         <Card id={movie.id} sx={{ display: 'flex', width: 400, margin: 5 }}>
             <CardMedia
                 component="img"
                 sx={{ width: 150, height: 225 }}
-                image="http://placekitten.com/500"
-                alt="Live from space album cover"
+                image={`${BASE_IMAGE_URL}${movie.poster_path}`}
+                alt="Movie poster"
             />
             <Box sx={{display: 'flex', flexDirection: 'column'}}>
                 <CardContent sx={{flex: '1 0 auto'}}>
