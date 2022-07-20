2276d6db6f58b31ddd508659bb959c1104e5039d
estherjanesamuel <esther.jane.samuel@gmail.com>
2022-07-20 14:38:07 +0700

    link nav using router

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
-import { ThemeProvider } from '@mui/material';
+import { Box,ThemeProvider } from '@mui/material';
 import Navbar from './components/Navbar';
 import theme from './themes/theme';
 import Movies from './containers/Movies';
-
+import {Routes,Route} from 'react-router-dom'
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <div className='App'>
         <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Movies />}></Route>
+          <Route path="about" element={<Box sx={{mt:10}}>Halaman About</Box>}></Route>
+          <Route path="indonesian" element={<Box sx={{mt:10}}>Halaman indonesian</Box>}></Route>
+          <Route path="pricing" element={<Box sx={{mt:10}}>Halaman pricing</Box>}></Route>
+        </Routes>
         <Movies></Movies>
       </div>
     </ThemeProvider>
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -26,7 +26,7 @@ const MovieCard = ({ movie }) => {
                     </Typography>
                     <Box sx={{width:200,display: 'flex',alignItems: 'center',}}>
                         <Rating name="read-only" precision={0.1} value={movie.vote_average / 2} max={5} readOnly />
-                        <Box sx={{ml:2}}>{movie.vote_average}</Box>
+                        <Box sx={{ml:2}}>{movie.vote_average.toFixed(1)}</Box>
                     </Box>
                 </CardContent>
             </Box>
