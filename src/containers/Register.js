70db4e819df1bd016890f62157eb1e7b9a19f1f3
estherjanesamuel <esther.jane.samuel@gmail.com>
2022-07-27 00:52:21 +0700

    fix register

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,34 +1,35 @@
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Avatar from "@mui/material/Avatar";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import TextField from "@mui/material/TextField";
-import Typography from "@mui/material/Typography";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { auth } from "../config/firebase";
+
+import { auth } from '../config/firebase';
 
 const Register = () => {
     const navigate = useNavigate();
     const [errorMessage, setErrorMessage] = useState('')
+
     const handleSubmit = async (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
         const email = data.get('email');
         const password = data.get('password');
-
+        
         try {
             const { user } = await createUserWithEmailAndPassword(auth, email, password);
             console.log(user);
             navigate("/");
-        } catch (err) {
-            setErrorMessage(err.message);
+        } catch (error) {
+            setErrorMessage(error.message);
         }
     };
-
 
     return (
         <Container component="main" maxWidth="xs">
@@ -46,13 +47,13 @@ const Register = () => {
                 <Typography component="h1" variant="h5">
                     Sign up
                 </Typography>
-                <Box component="from" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
                     <Grid container spacing={2}>
-
                         <Grid item xs={12}>
                             <TextField
                                 required
-                                fullWidthid="email"
+                                fullWidth
+                                id="email"
                                 label="Email Address"
                                 name="email"
                                 autoComplete="email"
@@ -63,7 +64,7 @@ const Register = () => {
                                 required
                                 fullWidth
                                 name="password"
-                                label="password"
+                                label="Password"
                                 type="password"
                                 id="password"
                                 autoComplete="new-password"
